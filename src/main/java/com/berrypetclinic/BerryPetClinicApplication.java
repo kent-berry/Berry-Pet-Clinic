d98041334ba422a515f84eeeaf52da3b8d955595
kentwberry4 <kentwberry@protonmail.com>
2022-12-28 16:12:01 -0500

    initial commit

---
+++ src/main/java/com/berrypetclinic/BerryPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.berrypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerryPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berrypetclinic/BerryPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berrypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerryPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
